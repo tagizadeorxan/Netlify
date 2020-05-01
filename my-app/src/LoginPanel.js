4ac8372f12a89144c02ba9634d6a5668769660cd
Tagizade Orxan <tagizade.orxan@gmail.com>
2020-05-01 19:47:53 +0400

    ok

--- my-app/src/LoginPanel.js
+++ my-app/src/LoginPanel.js
@@ -47,7 +47,7 @@ register = (e) => {
 
 
 checkData = (emailvalue,passwordvalue)=> {
-    fetch(`/getemail/${emailvalue}`).then(data=> data.json()).then(data => data.length<1 ? this.addData(emailvalue,passwordvalue) :alert(`sizin qeydiyyatiniz var`));
+    fetch(`http://localhost:3306/getemail/${emailvalue}`).then(data=> data.json()).then(data => data.length<1 ? this.addData(emailvalue,passwordvalue) :alert(`sizin qeydiyyatiniz var`));
 }
 
 
@@ -66,7 +66,7 @@ addData = (emailvalue,passwordvalue) => {
             password: passwordvalue
         })
     };
-    fetch("/register", options);
+    fetch("http://localhost:3306/register", options);
 
 
 
